ce57de8778779446ee32989493c1ac837bd58042
Mr STRATEGIST <142794511+tandavbot2@users.noreply.github.com>
2024-07-31 20:36:22 +0530

    Update commands.py

--- forwardbot/plugins/commands.py
+++ forwardbot/plugins/commands.py
@@ -1,7 +1,8 @@
+# commands.py
+
 from forwardbot import Config
 from telethon.tl.functions.users import GetFullUserRequest
-from forwardbot.utils import forwardbot_cmd
-from forwardbot.utils import is_sudo
+from forwardbot.utils import forwardbot_cmd, is_sudo
 from forwardbot import bot
 from ethon.mystarts import start_srb
 
@@ -14,11 +15,8 @@ sudo_users = Config.SUDO_USERS
 
 @forwardbot_cmd("start", is_args=False)
 async def start(event):
-    text=f"**ʜɪ ɪ ᴀᴍ ᴀ ᴘᴜʙʟɪᴄ ғᴏʀᴡᴀʀᴅᴇʀ ʙᴏᴛ.** \n**ᴜsɪɴɢ ᴍᴇ ʏᴏᴜ ᴄᴀɴ ғᴏʀᴡᴀʀᴅ ᴀʟʟ ᴛʜᴇ ғɪʟᴇs ɪɴ ᴀ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴀɴᴏᴛʜᴏʀ ᴇᴀsɪʟʏ** \n**ᴍʏ ᴀᴄᴄᴏᴜɴᴛ ɢᴇᴛ ʙᴀɴ ᴀɴʏᴛɪᴍᴇ ᴀɴᴅ ᴛʜɪs ʙᴏᴛ ᴀʟsᴏ, sᴏ ɪᴏɪɴ ᴏᴜʀ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/vj_botz) ғᴏʀ ɴᴇᴡ ʙᴏᴛ ᴜᴘᴅᴀᴛᴇ.**\n**/help ғᴏʀ ᴋɴᴏᴡ ᴍᴏʀᴇ ᴀʙᴏᴜᴛ ʙᴏᴛ**"
+    text = f"**ʜɪ ɪ ᴀᴍ ᴀ ᴘᴜʙʟɪᴄ ғᴏʀᴡᴀʀᴅᴇʀ ʙᴏᴛ.** \n**ᴜsɪɴɢ ᴍᴇ ʏᴏᴜ ᴄᴀɴ ғᴏʀᴡᴀʀᴅ ᴀʟʟ ᴛʜᴇ ғɪʟᴇs ɪɴ ᴀ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴀɴᴏᴛʜᴇʀ ᴇᴀsɪʟʏ** \n**ᴍʏ ᴀᴄᴄᴏᴜɴᴛ ɢᴇᴛ ʙᴀɴ ᴀɴʏᴛɪᴍᴇ ᴀɴᴅ ᴛʜɪs ʙᴏᴛ ᴀʟsᴏ, sᴏ ᴊᴏɪɴ ᴏᴜʀ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/vj_botz) ғᴏʀ ɴᴇᴡ ʙᴏᴛ ᴜᴘᴅᴀᴛᴇ.**\n**/help ғᴏʀ ᴋɴᴏᴡ ᴍᴏʀᴇ ᴀʙᴏᴜᴛ ʙᴏᴛ**"
     await start_srb(event, text)
-
-
-
 
 @forwardbot_cmd("help", is_args=False)
 async def handler(event):
@@ -28,13 +26,52 @@ async def handler(event):
 async def handler(event):
     await event.respond(bot.owner)
 
-
-
 @forwardbot_cmd("admin", is_args=False)
 async def handler(event):
     if str(event.sender_id) in sudo_users:
         await event.respond("You are an admin")
     else:
         await event.respond("You are not an admin")
+
+
+
+# from forwardbot import Config
+# from telethon.tl.functions.users import GetFullUserRequest
+# from forwardbot.utils import forwardbot_cmd
+# from forwardbot.utils import is_sudo
+# from forwardbot import bot
+# from ethon.mystarts import start_srb
+
+# MessageCount = 0
+
+# BOT_STATUS = "0"
+# status = set(int(x) for x in (BOT_STATUS).split())
+# help_msg = Config.HELP_MSG
+# sudo_users = Config.SUDO_USERS
+
+# @forwardbot_cmd("start", is_args=False)
+# async def start(event):
+#     text=f"**ʜɪ ɪ ᴀᴍ ᴀ ᴘᴜʙʟɪᴄ ғᴏʀᴡᴀʀᴅᴇʀ ʙᴏᴛ.** \n**ᴜsɪɴɢ ᴍᴇ ʏᴏᴜ ᴄᴀɴ ғᴏʀᴡᴀʀᴅ ᴀʟʟ ᴛʜᴇ ғɪʟᴇs ɪɴ ᴀ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴀɴᴏᴛʜᴏʀ ᴇᴀsɪʟʏ** \n**ᴍʏ ᴀᴄᴄᴏᴜɴᴛ ɢᴇᴛ ʙᴀɴ ᴀɴʏᴛɪᴍᴇ ᴀɴᴅ ᴛʜɪs ʙᴏᴛ ᴀʟsᴏ, sᴏ ɪᴏɪɴ ᴏᴜʀ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/vj_botz) ғᴏʀ ɴᴇᴡ ʙᴏᴛ ᴜᴘᴅᴀᴛᴇ.**\n**/help ғᴏʀ ᴋɴᴏᴡ ᴍᴏʀᴇ ᴀʙᴏᴜᴛ ʙᴏᴛ**"
+#     await start_srb(event, text)
+
+
+
+
+# @forwardbot_cmd("help", is_args=False)
+# async def handler(event):
+#     await event.respond(help_msg)
+
+# @forwardbot_cmd("test", is_args=False)
+# async def handler(event):
+#     await event.respond(bot.owner)
+
+
+
+# @forwardbot_cmd("admin", is_args=False)
+# async def handler(event):
+#     if str(event.sender_id) in sudo_users:
+#         await event.respond("You are an admin")
+#     else:
+#         await event.respond("You are not an admin")
 
 
