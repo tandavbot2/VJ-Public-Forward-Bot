d99ccea375a74a88ff6f07699eed506fc104a882
Mr STRATEGIST <142794511+tandavbot2@users.noreply.github.com>
2024-07-31 22:37:34 +0530

    Update __main__.py

--- forwardbot/__main__.py
+++ forwardbot/__main__.py
@@ -1,11 +1,11 @@
 from forwardbot.BotConfig import Config
-from forwardbot import bot
-from forwardbot import client
+from forwardbot import bot, client
 from pathlib import Path
 from sys import argv
 from forwardbot.utils import start_forwardbot
 import glob
 
+# Command-line argument handling
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
     client.disconnect()
@@ -13,17 +13,19 @@ else:
     bot.start(bot_token=Config.BOT_TOKEN)
     client.start()
 
+# Load plugins
 path = "forwardbot/plugins/*.py"
 files = glob.glob(path)
 for name in files:
     with open(name) as f:
         path1 = Path(f.name)
         shortname = path1.stem
-        start_forwardbot(shortname.replace(".py", ""))
+        start_forwardbot(shortname)
 
 print("Your BOT is Ready.")
 print("Try Sending /start")
 
+# Run client
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
     client.disconnect()
